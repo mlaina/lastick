2e70bfed02035681a0dd8bea1031d4e0293a4eb4
mlaina <laina855@gmail.com>
2017-09-27 19:13:45 +0200

    arc

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,29 @@ window.onload = function(){
     var curr = h * 60 * 60 + m * 60 + s;
     svg.setCurrentTime(curr);
 };
+
+//$("arc1").setAttribute("d", describeArc(200, 400, 100, 0, 180));
+
+function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
+  var angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
+
+  return {
+    x: centerX + (radius * Math.cos(angleInRadians)),
+    y: centerY + (radius * Math.sin(angleInRadians))
+  };
+}
+
+function describeArc(x, y, radius, startAngle, endAngle){
+
+    var start = polarToCartesian(x, y, radius, endAngle);
+    var end = polarToCartesian(x, y, radius, startAngle);
+
+    var largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1";
+
+    var d = [
+        "M", start.x, start.y, 
+        "A", radius, radius, 0, largeArcFlag, 0, end.x, end.y
+    ].join(" ");
+
+    return d;       
+}
