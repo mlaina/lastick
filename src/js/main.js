51a601eb991a9400d6b5b2d115be919658bbc938
mlaina <laina855@gmail.com>
2017-09-27 20:23:41 +0200

    rings

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,55 @@
 window.onload = function(){
+    /*
     var now = new Date();
     var h = now.getHours(), m = now.getMinutes(), s = now.getSeconds();
     var curr = h * 60 * 60 + m * 60 + s;
     svg.setCurrentTime(curr);
+    */
+
+    var a;
+
+    ah = [ 0, 0 ];
+    am = [ 0, 0 ];
+
+    ah=iniFinHours(12, 30, 13, 40);
+    am=iniFinMin(12, 30, 13, 40);
+    
+    $("#center").attr("d", describeArc(100, 100, 1, 0, 359));
+    $("#hour").attr("d", describeArc(100, 100, 100, ah[0], ah[1]));
+    $("#min").attr("d", describeArc(100, 100, 140, am[0], am[1]));
+
 };
 
-//$("arc1").setAttribute("d", describeArc(200, 400, 100, 0, 180));
+
+
+function iniFinMin( hi, mi, hf, mf ) {
+	var angleIni, angleFin;
+
+	var h=hf-hi;
+	var m=mi-mf;
+
+	
+
+	if(hf-hi>1|| (hi<hf && mi<mf && hf-hi==1)){
+		console.log(h+ " "+ m);
+		return [0, 359];
+	}
+
+	angleIni=mi*6;
+	angleFin=mf*6;
+
+	return [angleIni, angleFin];
+}
+
+function iniFinHours(hi, mi, hf, mf){
+	var angleIni, angleFin;
+
+	angleIni=(hi%12)*30+(30*mi/60);	
+	angleFin=(hf%12)*30+(30*mf/60);	
+
+	return  [angleIni, angleFin] ;
+}
+
 
 function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
   var angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
