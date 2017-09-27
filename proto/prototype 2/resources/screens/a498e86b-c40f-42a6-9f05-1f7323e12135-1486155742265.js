3e18d37e175cbab5928cce626ca62b6b9419625f
mlaina <laina855@gmail.com>
2017-09-27 17:17:42 +0200

    ini

---
+++ proto/prototype 2/resources/screens/a498e86b-c40f-42a6-9f05-1f7323e12135-1486155742265.js
@@ -0,0 +1,54 @@
+jQuery("#simulation")
+  .on("click", ".s-a498e86b-c40f-42a6-9f05-1f7323e12135 .click", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getEventFirer();
+    if(jFirer.is("#s-Hotspot_1")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/33f98097-4d21-4bcf-97fd-adb6591f308a"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Hotspot_2")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/33f98097-4d21-4bcf-97fd-adb6591f308a"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    }
+  });
---
+++ proto/prototype 2/resources/screens/acf98640-befb-4a3d-afa6-75017b108b90-1486155742265.js
@@ -0,0 +1,31 @@
+jQuery("#simulation")
+  .on("click", ".s-acf98640-befb-4a3d-afa6-75017b108b90 .click", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getEventFirer();
+    if(jFirer.is("#s-Hotspot_1")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/d12245cc-1680-458d-89dd-4f0d7fb22724"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    }
+  });
