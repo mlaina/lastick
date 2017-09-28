e288ed342409e8e36951579b9514cc526250cb45
mlaina <laina855@gmail.com>
2017-09-28 13:37:59 +0200

    bootstrap

--- src/js/main.js
+++ src/js/main.js
@@ -15,105 +15,18 @@ window.onload = function(){
     am=iniFinMin(12, 5, 12, 45);
     
     $("#center").attr("d", describeArc(100, 100, 1, 0, 359));
-    $("#hour").attr("d", describeArc(100, 100, 100, ah[0], ah[1]));
-    $("#min").attr("d", describeArc(100, 100, 140, am[0], am[1]));
+    $("#hour").attr("d", describeArc(100, 100, 140, ah[0], ah[1]));
+    $("#min").attr("d", describeArc(100, 100, 230, am[0], am[1]));
 
-    var colour1 = "#ffdc7b";
-    var colour2 = triangulateColour(colour1);
-    var colour3 = triangulateColour(colour2);
-    console.log("colour1: " + colour1);
-    console.log("colour2: " + colour2);
-    console.log("colour3: " + colour3);
+
+ 
 };
-
-/* hexToComplimentary : Converts hex value to HSL, shifts
- * hue by 180 degrees and then converts hex, giving complimentary color
- * as a hex value
- * @param  [String] hex : hex value  
- * @return [String] : complimentary color as hex value
- */
-function triangulateColour(hex){
-
-    // Convert hex to rgb
-    // Credit to Denis http://stackoverflow.com/a/36253499/4939630
-    var rgb = 'rgb(' + (hex = hex.replace('#', '')).match(new RegExp('(.{' + hex.length/3 + '})', 'g')).map(function(l) { return parseInt(hex.length%2 ? l+l : l, 16); }).join(',') + ')';
-
-    // Get array of RGB values
-    rgb = rgb.replace(/[^\d,]/g, '').split(',');
-
-    var r = rgb[0], g = rgb[1], b = rgb[2];
-
-    // Convert RGB to HSL
-    // Adapted from answer by 0x000f http://stackoverflow.com/a/34946092/4939630
-    r /= 255.0;
-    g /= 255.0;
-    b /= 255.0;
-    var max = Math.max(r, g, b);
-    var min = Math.min(r, g, b);
-    var h, s, l = (max + min) / 2.0;
-
-    if(max == min) {
-        h = s = 0;  //achromatic
-    } else {
-        var d = max - min;
-        s = (l > 0.5 ? d / (2.0 - max - min) : d / (max + min));
-
-        if(max == r && g >= b) {
-            h = 1.0472 * (g - b) / d ;
-        } else if(max == r && g < b) {
-            h = 1.0472 * (g - b) / d + 6.2832;
-        } else if(max == g) {
-            h = 1.0472 * (b - r) / d + 2.0944;
-        } else if(max == b) {
-            h = 1.0472 * (r - g) / d + 4.1888;
-        }
-    }
-
-    h = h / 6.2832 * 360.0 + 0;
-
-    // Shift hue to opposite side of wheel and convert to [0-1] value
-    h+= 120;
-    if (h > 360) { h -= 360; }
-    h /= 360;
-
-    // Convert h s and l values into r g and b values
-    // Adapted from answer by Mohsen http://stackoverflow.com/a/9493060/4939630
-    if(s === 0){
-        r = g = b = l; // achromatic
-    } else {
-        var hue2rgb = function hue2rgb(p, q, t){
-            if(t < 0) t += 1;
-            if(t > 1) t -= 1;
-            if(t < 1/6) return p + (q - p) * 6 * t;
-            if(t < 1/2) return q;
-            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
-            return p;
-        };
-
-        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-        var p = 2 * l - q;
-
-        r = hue2rgb(p, q, h + 1/3);
-        g = hue2rgb(p, q, h);
-        b = hue2rgb(p, q, h - 1/3);
-    }
-
-    r = Math.round(r * 255);
-    g = Math.round(g * 255); 
-    b = Math.round(b * 255);
-
-    // Convert r b and g values to hex
-    rgb = b | (g << 8) | (r << 16); 
-    return "#" + (0x1000000 | rgb).toString(16).substring(1);
-}  
 
 function iniFinMin( hi, mi, hf, mf ) {
 	var angleIni, angleFin;
 
 	var h=hf-hi;
 	var m=mi-mf;
-
-	
 
 	if(hf-hi>1|| (hi<hf && mi<mf && hf-hi==1)){
 		console.log(h+ " "+ m);
